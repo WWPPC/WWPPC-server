35d83908aac53fb8cb11fc9548b281cc83803431
erikji <temple.monkey.god@gmail.com>
2024-03-05 18:57:50 -0500

    buh

--- wwppc-client/src/pages/home/PagePanelHomeHome.vue
+++ wwppc-client/src/pages/home/PagePanelHomeHome.vue
@@ -5,23 +5,26 @@ import LargeGlitchCenterText from '@/components/LargeGlitchCenterText.vue';
 <template>
     <div class="fullBlock">
         <br><br>
-        <LargeGlitchCenterText text="WWP-IT + WWP-HACKS" fontStyle="
-             font-weight:bold;
+        <LargeGlitchCenterText text="WWPPC 2024" fontStyle="
+            font-weight:bold;
             font-size: min(calc(50vh - 50px), calc(100vw / 12));
             text-wrap: nowrap;
             color: lime;
             text-shadow: -0.05em 0.05em 0px #0F05, 0px 0px 0.02em #0F0;
             " :steps=1 :delay=10></LargeGlitchCenterText>
         <br>
-        <div class="center">
-            <!-- show signup, date, etc also arrow -->
+        <LargeGlitchCenterText text="??/??" fontStyle="
+             font-weight:bold;
+            font-size: min(calc(50vh - 50px), calc(100vw / 12));
+            text-wrap: nowrap;
+            color: red;
+            " :steps=1 :delay=10></LargeGlitchCenterText>
+        <br>
+        <div class="center" style="text-align: center">
+            West Windsor-Plainsboro Programming Competition (WWPPC) will be held eventually in 2024. Join us on <a href="https://discord.gg/rv23HbH7rB" style="color: lime;">Discord</a>! 
         </div>
     </div>
     <div class="fullBlock">
-        wow thing
-        <br><br>
-        pretend this is a discord logo JOIN OUR DISCORD! https://discord.gg/97um6csDGn
-        <br>
-        I should probably make this into a component
+        
     </div>
 </template>
