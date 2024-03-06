ab5d1b3ed0de22c4e0a1561e7b6e3542a804157b
erikji <temple.monkey.god@gmail.com>
2024-03-05 20:21:20 -0500

    changed a LOT of stuff!!!

--- wwppc-client/src/pages/home/PagePanelHomeAbout.vue
+++ wwppc-client/src/pages/home/PagePanelHomeAbout.vue
@@ -2,5 +2,22 @@
 </script>
 
 <template>
-    hi
+    Our Team
+    <table>
+        <tr>
+            <td>Arav Maheria</td>
+            <td>Erik Ji</td>
+        </tr>
+        <tr>
+            <td>Jie Gao</td>
+            <td>Maitian Sha</td>
+        </tr>
+        <tr>
+            <td>Rishikesh Anabathula</td>
+            <td>Suvanth Erranki</td>
+        </tr>
+        <tr>
+            <td>Vikram Karamsetty</td>
+        </tr>
+    </table>
 </template>
