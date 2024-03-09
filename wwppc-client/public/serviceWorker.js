a4c8dee42bbc3350fced4f08124242a43049a172
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-03-08 22:05:58 -0500

    add csp and service worker (sort of again)
    content security policy moment

---
+++ wwppc-client/public/serviceWorker.js
@@ -0,0 +1,77 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(new Promise(async (resolve) => {
+        const pageCache = await caches.open('page');
+        await pageCache.addAll([
+            '/',
+            '/index.html',
+            '/favicon.png',
+            '/logo.svg',
+            '/icon.svg',
+            '/icon2.png'
+        ]);
+        self.skipWaiting();
+        resolve();
+    }))
+});
+self.addEventListener("activate", (e) => {
+    let activate = async () => {
+        await self.registration.navigationPreload?.enable();
+        await self.clients.claim();
+    };
+    e.waitUntil(activate());
+});
+let getCached = async (request, preloadResponse) => {
+    try {
+        const cache = await caches.open('page');
+        // serve from cache while also updating the cache if possible
+        const cached = await cache.match(request);
+        if (cached !== undefined) {
+            updateCache(cache, request, undefined);
+            return cached;
+        } else {
+            return await updateCache(cache, request, preloadResponse);
+        }
+    } catch (err) {
+        console.error(err);
+        return new Response('cache error', {
+            status: 502,
+            headers: { "Content-Type": "text/plain" }
+        });
+    }
+};
+let updateCache = async (cache, request, preloadResponse) => {
+    try {
+        const preloaded = await preloadResponse;
+        if (preloaded !== undefined && preloaded.ok) {
+            cache.put(request.url, preloaded.clone());
+            return preloaded;
+        }
+    } catch { 0 }
+    try {
+        const networked = await fetch(request);
+        if (networked.ok) cache.put(request.url, networked.clone());
+        return networked;
+    } catch (err) {
+        if (preloadResponse != undefined) console.error(err);
+        return new Response('timed out', {
+            status: 408,
+            headers: { "Content-Type": "text/plain" }
+        });
+    }
+};
+self.addEventListener("fetch", (e) => {
+    if (e.request.method != 'GET') return;
+    if (e.request.url.startsWith(self.location.origin)) {
+        e.respondWith(getCached(e.request, e.preloadResponse));
+    } else {
+        try {
+            e.respondWith(fetch(e.request));
+        } catch (err) {
+            console.error(err);
+            return new Response('timed out', {
+                status: 408,
+                headers: { "Content-Type": "text/plain" }
+            });
+        }
+    }
+});
