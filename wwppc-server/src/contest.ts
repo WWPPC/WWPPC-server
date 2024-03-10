2b56b1941e9cc972377a0e4dee97ba694eaadf01
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-03-10 18:51:05 -0400

    a

--- wwppc-server/src/contest.ts
+++ wwppc-server/src/contest.ts
@@ -9,7 +9,7 @@ export class ContestManager {
     // private database: Database = new Database("tempURL", "tempKey", this.logger);
     problemCache: Array<Array<Problem>> | undefined;
     roundNum = 0;
-    static readonly FILE_SIZE_CAP = 1e4; 
+    static readonly FILE_SIZE_CAP = 1e4;
     // all socketio connections are put here (IN A SET NOT AN ARRAY)
     // start/stop rounds, control which problems are where
     // uses database to get problems and then caches them
@@ -20,7 +20,7 @@ export class ContestManager {
     //Added problem cache
 
     //TODO: Create round blocks, add socket io and single connection
-    
+
     /**
      * 
      * @param username Username of submitter for database purposes
@@ -28,32 +28,40 @@ export class ContestManager {
      * @param file File submission
      */
 
-    async getNextRound(division: string){
+    async getNextRound(division: string) {
         this.roundNum++;
         // let stuff: Array<Problem> = await this.database.readProblems({id: "*", division: division, round: this.roundNum.toString(), number: "*", name: "*", author: "*", constraints: constraints => {return true}});
         // this.problemCache?.push(stuff);
         // return stuff;
     }
 
-    async getRound(division: string, round: number){
-        if(this.problemCache?.length === undefined){
+    async getRound(division: string, round: number) {
+        if (this.problemCache?.length === undefined) {
             this.getNextRound(division);
         }
         // @ts-ignore
-        while(this.problemCache?.length <= round){
+        while (this.problemCache?.length <= round) {
             this.getNextRound(division);
         }
         return this.problemCache?.at(round);
     }
 
-    async submitFile(username: string, problem: Problem, file: File) : Promise<boolean>{
-        if(file.size > ContestManager.FILE_SIZE_CAP){
+    async submitFile(username: string, problem: Problem, file: File): Promise<boolean> {
+        if (file.size > ContestManager.FILE_SIZE_CAP) {
             return false;
         }
 
         const scores: Score[] = this.scoreSubmission(problem, file)
-        const s: Submission = {username: username, division: problem.division, round: problem.round, number: problem.number, time: Date.now(),
-            file: await file.text(), lang: this.getLang(file), scores: scores};
+        const s: Submission = {
+            username: username,
+            division: problem.division,
+            round: problem.round,
+            number: problem.number,
+            time: Date.now(),
+            file: await file.text(),
+            lang: this.getLang(file),
+            scores: scores
+        };
         // this.database.writeSubmission(s);
         return true;
     }
@@ -63,7 +71,7 @@ export class ContestManager {
         return [];
     }
 
-    public getLang(file: File) : string{
+    public getLang(file: File): string {
         //do we use domjudge for this?
         return "";
     }
