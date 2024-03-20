e1d3b69597231ac28d1acb68c177bd7243550460
erikji <temple.monkey.god@gmail.com>
2024-03-19 21:05:31 -0400

    fix wrong text size, fix nowrap not working button

--- wwppc-client/src/components/ui-defaults/inputs/UIIconButton.vue
+++ wwppc-client/src/components/ui-defaults/inputs/UIIconButton.vue
@@ -68,6 +68,7 @@ watch(() => props.text, () => buttonText.value = props.text);
 .uiIconButtonText {
     margin: 0px 0.2em;
     text-wrap: nowrap;
+    white-space: nowrap;
     font-size: v-bind("$props.fontSize ?? 'var(--font-16)'");
 }
 
