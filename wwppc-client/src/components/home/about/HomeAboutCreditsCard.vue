75507f3167752fcfcedbeb32a523b71c1b0c3889
erikji <temple.monkey.god@gmail.com>
2024-03-23 18:50:25 -0400

    add github codeforces icons

--- wwppc-client/src/components/home/about/HomeAboutCreditsCard.vue
+++ wwppc-client/src/components/home/about/HomeAboutCreditsCard.vue
@@ -1,20 +1,32 @@
 <script setup lang="ts">
 import { AnimateInContainer, DoubleCutCornerContainer } from '@/components/ui-defaults/UIContainers';
+import { UIIconButton } from '@/components/ui-defaults/UIDefaults';
 
 defineProps<{
     name: string,
     roles: string,
+    codeforces?: string,
+    github?: string,
     img: string
 }>();
 </script>
 
 <template>
     <AnimateInContainer type="slideUp" show-on-screen single>
-        <DoubleCutCornerContainer hover-animation="lift">
+        <DoubleCutCornerContainer hover-animation="lift" height="100%">
             <div class="homeAboutCreditsCardContent">
                 <img :src=$props.img class="homeAboutCreditsCardImage">
                 <div class="homeAboutCreditsCardName">{{ $props.name }}</div>
                 <div class="homeAboutCreditsCardRoles" v-html=$props.roles></div>
+            </div>
+            <div class="homeAboutCreditsCardIcons">
+                <!-- Maybe we should create a reusable component for buttons with external links -->
+                <a class="homeAboutCreditsCardIconLink" :href="'https://codeforces.com/profile/' + codeforces" target="_blank" v-if="codeforces != undefined && codeforces != null">
+                    <img class="homeAboutCreditsCardIconImage" src="/assets/codeforces.svg"/>
+                </a>
+                <a class="homeAboutCreditsCardIconLink" :href="'https://github.com/' + github" target="_blank" v-if="github != undefined && github != null">
+                    <img class="homeAboutCreditsCardIconImage" src="/assets/github.svg"/>
+                </a>
             </div>
         </DoubleCutCornerContainer>
     </AnimateInContainer>
@@ -35,7 +47,7 @@ defineProps<{
 }
 .homeAboutCreditsCardName {
     margin-top: 8px;
-    width: 280px;
+    width: 100%;
     text-align: center;
     text-wrap: nowrap;
     font-size: var(--font-24);
@@ -44,8 +56,18 @@ defineProps<{
 }
 .homeAboutCreditsCardRoles {
     text-align: center;
-    padding-top: 18px;
+    padding-top: var(--font-18);
     font-size: var(--font-18);
     color: lime;
+}
+.homeAboutCreditsCardIcons {
+    display: flex;
+    justify-content: space-evenly;
+}
+.homeAboutCreditsCardIconLink {
+    text-decoration: none;
+}
+.homeAboutCreditsCardIconImage {
+    height: var(--font-32);
 }
 </style>
