ea1ed9756e94b67a916c9f33afb45ba7a746232a
erikji <temple.monkey.god@gmail.com>
2024-04-02 15:55:22 -0400

    buh

--- wwppc-server/src/contest.ts
+++ wwppc-server/src/contest.ts
@@ -1,5 +1,5 @@
 import { Socket } from "socket.io";
-import { AccountData } from "./database";
+import { AccountData, Submission } from "./database";
 
 /**
  * 
@@ -10,6 +10,9 @@ export class ContestManager {
     // responsible for serving round data and problem data
     // the user must be signed in and registered for the contest and division of the round/problem AND THE ROUND HAS TO BE ACTIVE
 
+    #submissionQueue: unknown; // make this a queue or something idk
+    // queue of submissions, will be popped from when /api/v4/judgehosts/fetch-work is called
+
     // all socketio connections are put here (IN A SET NOT AN ARRAY)
     // start/stop rounds, control which problems are where
     // uses database to get problems and then caches them (also stores division, round, number since client needs that)
@@ -17,6 +20,16 @@ export class ContestManager {
     // remember to prevent large file submissions (over 10kb is probably unnecessarily large for these problems)
     // use socket.io rooms
     // 
+
+    /**
+     * Queue a submission to be judged
+     * @param {Submission} submission submission
+     * @param user user that made the submission
+     * @returns {boolean} whether the thing was successfully pushed to the queue
+     */
+    queueSubmission(user: string, submission: Submission) {
+        return true;
+    }
 }
 
 export default ContestManager;
