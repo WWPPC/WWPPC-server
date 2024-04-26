d5124f14ca06353a7f75d3b4ed8639b8eeee80b1
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-04-26 19:03:53 -0400

    wait i didnt save oops

--- wwppc-client/src/scripts/ContestManager.ts
+++ wwppc-client/src/scripts/ContestManager.ts
@@ -83,23 +83,6 @@ export interface ArchiveProblem {
     constraints: { memory: number, time: number }
 }
 
-export const completionStateAnimation = (status: ContestProblemCompletionState) => {
-    return status == ContestProblemCompletionState.NOT_UPLOADED ? 'pstatus-not-uploaded' :
-        status == ContestProblemCompletionState.UPLOADED ? 'pstatus-uploaded' :
-            status == ContestProblemCompletionState.SUBMITTED ? 'pstatus-submitted' :
-                status == ContestProblemCompletionState.GRADED_PASS ? 'pstatus-graded-pass' :
-                    status == ContestProblemCompletionState.GRADED_FAIL ? 'pstatus-graded-fail' :
-                        status == ContestProblemCompletionState.GRADED_PARTIAL ? 'pstatus-graded-partial' : 'pstatus-error'
-};
-export const completionStateString = (status: ContestProblemCompletionState) => {
-    return status == ContestProblemCompletionState.NOT_UPLOADED ? 'Not uploaded' :
-        status == ContestProblemCompletionState.UPLOADED ? 'Uploaded' :
-            status == ContestProblemCompletionState.SUBMITTED ? 'Submitted' :
-                status == ContestProblemCompletionState.GRADED_PASS ? 'Accepted' :
-                    status == ContestProblemCompletionState.GRADED_FAIL ? 'Failed' :
-                        status == ContestProblemCompletionState.GRADED_PARTIAL ? 'Partially accepted' : 'Error fetching status'
-};
-
 const state = reactive<{
     inContest: boolean,
     currContest: string
