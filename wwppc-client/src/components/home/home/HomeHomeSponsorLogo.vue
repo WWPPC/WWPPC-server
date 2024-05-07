e7dd22744368c2bf8bf555b599aae2eced1c255a
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-05-07 19:21:24 -0400

    oof bouncy animation

--- wwppc-client/src/components/home/home/HomeHomeSponsorLogo.vue
+++ wwppc-client/src/components/home/home/HomeHomeSponsorLogo.vue
@@ -16,7 +16,7 @@ defineProps<{
     width: 100%;
     height: 100%;
     filter: grayscale(1) brightness(1000) contrast(1000);
-    transition: 150ms cubic-bezier(0.5, 0.5, 0.5, 1) transform, 50ms linear filter;
+    transition: 150ms cubic-bezier(0.5, 0.5, 0.5, 2) transform, 50ms linear filter;
     transform: none;
     object-fit: contain;
     object-position: center;
