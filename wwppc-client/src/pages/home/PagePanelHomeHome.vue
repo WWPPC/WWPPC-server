5beea429c3fffa88ea740aaf7a806bbb53849243
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-05-28 22:54:03 -0400

    fix massive font

--- wwppc-client/src/pages/home/PagePanelHomeHome.vue
+++ wwppc-client/src/pages/home/PagePanelHomeHome.vue
@@ -141,22 +141,22 @@ import { nextContest, nextContestEnd } from "@/scripts/ContestManager";
         </div>
         <br>
         <div class="centered">
-            <GlitchText text="Silver" font-size="var(--font-title)" color="lime" glow shadow random :steps=2 on-visible></GlitchText>
+            <GlitchText text="Silver" font-size="var(--font-subtitle)" color="#EEE" glow shadow random :steps=2 on-visible></GlitchText>
         </div>
         <div class="sponsors">
-            <HomeHomeSponsorLogo src="/img/icode-logo.png" url="https://icodeschool.com/" height="15vh"></HomeHomeSponsorLogo>
-            <HomeHomeSponsorLogo src="/img/x-camp-logo.png" url="https://x-camp.academy/" height="15vh"></HomeHomeSponsorLogo>
+            <HomeHomeSponsorLogo src="/img/icode-logo.png" url="https://icodeschool.com/" height="12vh"></HomeHomeSponsorLogo>
+            <HomeHomeSponsorLogo src="/img/x-camp-logo.png" url="https://x-camp.academy/" height="12vh"></HomeHomeSponsorLogo>
         </div>
         <br>
         <div class="centered">
-            <GlitchText text="Bronze" font-size="var(--font-title)" color="lime" glow shadow random :steps=2 on-visible></GlitchText>
+            <GlitchText text="Bronze" font-size="var(--font-subtitle)" color="#EA0" glow shadow random :steps=2 on-visible></GlitchText>
         </div>
         <div class="sponsors">
-            <HomeHomeSponsorLogo src="/img/desmos-logo.svg" url="https://desmos.com/" height="8vh"></HomeHomeSponsorLogo>
-            <HomeHomeSponsorLogo src="/img/aops-logo.svg" url="https://artofproblemsolving.com/" height="16vh"></HomeHomeSponsorLogo>
-            <HomeHomeSponsorLogo src="/img/xyz-logo.svg" url="https://gen.xyz" height="12vh"></HomeHomeSponsorLogo>
-            <HomeHomeSponsorLogo src="/img/echo3d-logo.png" url="https://www.echo3d.com/" height="8vh"></HomeHomeSponsorLogo>
-            <HomeHomeSponsorLogo src="/img/axure-logo.svg" url="https://axure.com/" height="8vh"></HomeHomeSponsorLogo>
+            <HomeHomeSponsorLogo src="/img/desmos-logo.svg" url="https://desmos.com/" height="6vh"></HomeHomeSponsorLogo>
+            <HomeHomeSponsorLogo src="/img/aops-logo.svg" url="https://artofproblemsolving.com/" height="14vh"></HomeHomeSponsorLogo>
+            <HomeHomeSponsorLogo src="/img/xyz-logo.svg" url="https://gen.xyz" height="10vh"></HomeHomeSponsorLogo>
+            <HomeHomeSponsorLogo src="/img/echo3d-logo.png" url="https://www.echo3d.com/" height="6vh"></HomeHomeSponsorLogo>
+            <HomeHomeSponsorLogo src="/img/axure-logo.svg" url="https://axure.com/" height="6vh"></HomeHomeSponsorLogo>
         </div>
         <div style="flex-grow: 1;"></div>
         <ContactFooter></ContactFooter>
