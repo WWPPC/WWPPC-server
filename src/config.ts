37a2418fa208b0108ae2894608d6ad99f1a827f1
erikji <temple.monkey.god@gmail.com>
2025-04-18 10:56:05 -0400

    change name buh

--- src/config.ts
+++ src/config.ts
@@ -89,7 +89,7 @@ export interface ContestConfiguration {
     readonly submitSolver: boolean
     /**Submissions when {@link ContestConfiguration.submitSolver} is "false" will be delayed by a number of seconds (usually to discourage spamming) (default: 10) */
     readonly directSubmissionDelay: number
-    /**Programming languages accepted for submissions (case sensitive, only if "submitSolver" is "true") (default: Java8, Java11, Java17, Java21, C11, C++11, C++17, C++20, Python3.12.3, Pypy3) */
+    /**Programming languages accepted for submissions (case sensitive, only if "submitSolver" is "true") (default: Java8, Java11, Java17, Java21, C11, C++11, C++17, C++20, Python3.12.3, PyPy7.3.17) */
     readonly acceptedSolverLanguages: string[]
     /**Maximum character length of uploaded submissions (default: 10240) */
     readonly maxSubmissionSize: number
@@ -132,7 +132,7 @@ const config: GlobalConfiguration = {
                 'C++17',
                 'C++20',
                 'Python3.12.3',
-                'Pypy3'
+                'PyPy7.3.17'
             ],
             maxSubmissionSize: cVal.maxSubmissionSize ?? 10240
         };
